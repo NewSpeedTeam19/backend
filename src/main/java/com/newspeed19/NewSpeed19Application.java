dc81ac2cb8c3c59240bed2d5211a8ac75db2cedc
YongLeeCode <yonghwan311@gmail.com>
2025-04-07 12:25:13 +0900

    feat: create sns website project

---
+++ src/main/java/com/newspeed19/NewSpeed19Application.java
@@ -0,0 +1,13 @@
+package com.newspeed19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpeed19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSpeed19Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/newspeed19/NewSpeed19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newspeed19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSpeed19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
