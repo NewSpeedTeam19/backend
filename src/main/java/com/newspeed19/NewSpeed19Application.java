010a7af069ff289b7af9a6673468350118f616dd
wannabeing <wannabeing@naver.com>
2025-04-08 21:58:31 +0900

    feat: Feed 엔티티 클래스 생성

--- src/main/java/com/newspeed19/NewSpeed19Application.java
+++ src/main/java/com/newspeed19/NewSpeed19Application.java
@@ -2,14 +2,12 @@ package com.newspeed19;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class NewSpeed19Application {
-
-    public static void main(String[] args) {
-        System.out.println("test");
-
-        SpringApplication.run(NewSpeed19Application.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(NewSpeed19Application.class, args);
+	}
 }
