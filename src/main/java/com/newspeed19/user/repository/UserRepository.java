9eb73e9f8f30117093e8f922275cd2ed9d83160a
pokerbearkr <ogdongwon@gmail.com>
2025-04-08 10:44:39 +0900

    refactor : user entity, repository 위치 변경

--- src/main/java/com/newspeed19/user/repository/UserRepository.java
+++ src/main/java/com/newspeed19/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.newspeed19.profile.repository;
+package com.newspeed19.user.repository;
 
-import com.newspeed19.common.User;
+import com.newspeed19.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
