bfe821275ba9f1ad8ad2870e1331b2665c954159
sun <hsunvirus@gmail.com>
2025-04-08 19:41:43 +0900

    [FEAT] Uplaod Comment entity

---
+++ src/main/java/com/newspeed19/comment/entity/Comment.java
@@ -0,0 +1,38 @@
+package com.newspeed19.comment.entity;
+
+import com.newspeed19.common.entity.BaseEntity;
+import com.newspeed19.feed.entity.Feed;
+import com.newspeed19.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "comments")
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id", nullable = false)
+    private Feed feed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public void update(String content){
+        this.content = content;
+    }
+
+}
