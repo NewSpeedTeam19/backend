14199d949e6991e6caa5de5c939f7be78479953b
Tcimel <61877802+Tcimel@users.noreply.github.com>
2025-04-09 20:17:05 +0900

    [feat] upload baseentity (#16)

---
+++ src/main/java/com/newspeed19/common/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.newspeed19.common.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
