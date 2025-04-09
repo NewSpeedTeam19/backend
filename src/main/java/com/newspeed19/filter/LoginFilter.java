e83841a01ce22703a3a35192794fcd7a7ad0ba94
Yong Lee <yonghwan311@gmail.com>
2025-04-09 21:22:49 +0900

    User (#18)
    * test
    
    * feat: 로그인 필터 생성
    
    * refactor: 유저 서비스 수정

---
+++ src/main/java/com/newspeed19/filter/LoginFilter.java
@@ -0,0 +1,26 @@
+package com.newspeed19.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+/**
+ * @packageName    : com.newspeed19.filter
+ * @fileName       : LoginFilter
+ * @author         : yong
+ * @date           : 4/8/25
+ * @description    :
+ */
+public class LoginFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws
+		IOException,
+		ServletException {
+		// if(!)
+	}
+}
