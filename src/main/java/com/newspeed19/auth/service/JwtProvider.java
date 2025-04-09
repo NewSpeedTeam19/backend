7cb0c8789c5492386becc1d8b7f394e65bdb0b07
YongLeeCode <yonghwan311@gmail.com>
2025-04-09 19:40:02 +0900

    feat: 라이브러리 추가 및 auth 도메인에 필요한 파일 생성

---
+++ src/main/java/com/newspeed19/auth/service/JwtProvider.java
@@ -0,0 +1,78 @@
+package com.newspeed19.auth.service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+/**
+ * @packageName    : com.newspeed19.auth.service
+ * @fileName       : JwtProvider
+ * @author         : yong
+ * @date           : 4/9/25
+ * @description    :
+ */
+@Component
+public class JwtProvider {
+	@Value("${jwt.secret}")
+	private String secretKey;
+
+	private final long ACCESS_TOKEN_VALIDITY = 1000L * 60; // 1분
+
+	public String createToken(long userId, String userType) {
+		Claims claims = Jwts.claims().setSubject(String.valueOf(userId));
+		Date now = new Date();
+		Date expiration = new Date(now.getTime() + ACCESS_TOKEN_VALIDITY);
+
+		Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+		return Jwts.builder()
+			.setClaims(claims)
+			.setIssuedAt(now)
+			.setExpiration(expiration)
+			.signWith(key, SignatureAlgorithm.HS512)
+			.compact();
+	}
+
+	public String getUserId(String token) {
+		return Jwts.parserBuilder()
+			.setSigningKey(secretKey)
+			.build()
+			.parseClaimsJws(token)
+			.getBody()
+			.getSubject();
+	}
+
+	public boolean validateToken(String token) {
+		Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+		Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+		if (claims.isEmpty()) {
+			throw new RuntimeException("validate실패");
+		}
+		return true;
+
+	}
+
+	public String getToken(String authHeader) {
+		return authHeader.substring(7);
+	}
+
+	public long getExiration(String token) {
+		Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+		Claims claims = Jwts.parserBuilder()
+			.setSigningKey(key)
+			.build()
+			.parseClaimsJws(token)
+			.getBody();
+
+		Date expiration = claims.getExpiration();
+		return expiration.getTime();
+	}
+}
