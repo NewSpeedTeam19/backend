e1446e7067fc03097d446473ef8e56f2a29d85b0
sun <hsunvirus@gmail.com>
2025-04-10 18:06:58 +0900

    [feat] remove likes when deleting feed or comment

--- src/main/java/com/newspeed19/feed/entity/Feed.java
+++ src/main/java/com/newspeed19/feed/entity/Feed.java
@@ -1,14 +1,18 @@
 package com.newspeed19.feed.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.newspeed19.comment.entity.CommentLike;
 import com.newspeed19.common.entity.BaseEntity;
 import com.newspeed19.user.entity.User;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -18,6 +22,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
@@ -44,6 +49,9 @@ public class Feed extends BaseEntity {
 	@JoinColumn(nullable = false, name = "user_id")
 	@Setter
 	private User user;
+
+	@OneToMany(mappedBy = "feed", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<FeedLike> feedLikes = new ArrayList<>();
 
 	/**
 	 * ✅ Builder 생성자
