fdde00d55ec6ece2055a9d88569ff55cf56fc425
wannabeing <wannabeing@naver.com>
2025-04-11 00:03:35 +0900

    feat: Feed 예외코드 추가

--- src/main/java/com/newspeed19/feed/exception/FeedErrorCode.java
+++ src/main/java/com/newspeed19/feed/exception/FeedErrorCode.java
@@ -6,10 +6,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum FeedErrorCode {
-	/**
-	 * 3000: Feed 에러
-	 */
-	FEED_NOT_FOUND(404, "존재하지 않는 피드입니다.");
+	FEED_NOT_FOUND(404, "존재하지 않는 피드입니다."),
+	FEED_FORBIDDEN(403, "피드에 권한이 없습니다."),
+	FEED_MY_FEED_NO_LIKES(400, "본인 글에 좋아요는 불가능합니다.");
 
 	private final int code;
 	private final String message;
