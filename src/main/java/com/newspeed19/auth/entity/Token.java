d862035ca44d5b6cccf868d6b2b57a2e2224bedf
YongLeeCode <yonghwan311@gmail.com>
2025-04-10 12:49:50 +0900

    refactor: WHITE LIST 일부가 작동하지 않아 수정

---
+++ src/main/java/com/newspeed19/auth/entity/Token.java
@@ -0,0 +1,10 @@
+package com.newspeed19.auth.entity;
+
+/**
+ * @packageName    : com.newspeed19.auth.entity
+ * @fileName       : Token
+ * @author         : yong
+ * @date           : 4/10/25
+ * @description    :
+ */public class Token {
+}
