f307e1f3893007aaff23439516b535eab765e1b7
ChoiHyuk <wannabeing@naver.com>
2025-04-11 10:07:36 +0900

    [FEAT] Feed 목록 좋아요 내림차순 정렬기능 추가 (#36)
    * [feat] add feedlike feature
    
    * [feat] remove likes when deleting feed or comment
    
    * [REFACTOR] 유저조회 service단으로 이동 (#31)
    
    * feat: 피드 기간별 검색기능 추가 (#33)
    
    * [FEAT] Auth 도메인 예외처리 기능 구현 (#34)
    
    * test
    
    * feat: 로그인 필터 생성
    
    * feat: 예외처리 기능 구현
    
    * feat: Feed 예외코드 추가
    
    * feat: Feed 좋아요 기준 정렬하여 조회하는 기능 추가
    
    ---------
    
    Co-authored-by: sun <hsunvirus@gmail.com>
    Co-authored-by: dongwon oh <ogdongwon@gmail.com>
    Co-authored-by: Yong Lee <yonghwan311@gmail.com>

--- src/main/java/com/newspeed19/feed/exception/FeedErrorCode.java
+++ src/main/java/com/newspeed19/feed/exception/FeedErrorCode.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum FeedErrorCode {
 	FEED_NOT_FOUND(404, "존재하지 않는 피드입니다."),
-	FEED_UNAUTHORIZED(403, "해당 피드에 권한이 없습니다.");
+	FEED_FORBIDDEN(403, "피드에 권한이 없습니다."),
+	FEED_MY_FEED_NO_LIKES(400, "본인 글에 좋아요는 불가능합니다.");
 
 	private final int code;
 	private final String message;
--- src/main/java/com/newspeed19/feed/repository/FeedLikeRepository.java
+++ src/main/java/com/newspeed19/feed/repository/FeedLikeRepository.java
@@ -4,14 +4,31 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.newspeed19.comment.entity.Comment;
-import com.newspeed19.comment.entity.CommentLike;
 import com.newspeed19.feed.entity.Feed;
 import com.newspeed19.feed.entity.FeedLike;
 import com.newspeed19.user.entity.User;
 
 public interface FeedLikeRepository extends JpaRepository<FeedLike, Long> {
+	/**
+	 * [Repo] 유저가 해당 피드 좋아요를 눌렀는지 여부를 반환하는 메서드
+	 * @param user 유저 객체
+	 * @param feed 피드 객체
+	 * @return 좋아요 눌렀는지 여부를 반환 (true/false)
+	 */
 	boolean existsByUserAndFeed(User user, Feed feed);
+
+	/**
+	 * [Repo] 유저가 피드에 좋아요를 눌렀는지 조회하는 메서드
+	 * @param user 유저 객체
+	 * @param feed 피드 객체
+	 * @return 유저가 피드 누른 결과를 반환
+	 */
 	Optional<FeedLike> findByUserAndFeed(User user, Feed feed);
-	long countByFeedId(Long commentId);
+
+	/**
+	 * [Repo] 피드의 좋아요 개수를 카운트하는 메서드
+	 * @param feedId 피드 id
+	 * @return 피드 좋아요 개수를 반환
+	 */
+	Long countByFeedId(Long feedId);
 }
